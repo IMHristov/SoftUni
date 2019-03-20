69f8ba50bd25ed82d82907092bb73ed67e8dd4a6
IMHristov <40794067+IMHristov@users.noreply.github.com>
2019-03-21 01:06:50 +0200

    Add files via upload

---
+++ OOP Basic/Polymorphism/Vehicle1/Truck.cs
@@ -0,0 +1,18 @@
+﻿namespace _1.Vehicles
+{
+    public class Truck : Vehicle
+    {
+        private const double takenFuelInCharging = 0.95;
+
+        public Truck(double fuelAmount, double fuelConsumption)
+            : base(fuelAmount, fuelConsumption)
+        {
+        }
+
+        public override void Refuel(double fuel)
+        {
+            fuel *= takenFuelInCharging;
+            base.Refuel(fuel);
+        }
+    }
+}
---
+++ OOP Basic/Polymorphism/Vehicle1/Vehicle.cs
@@ -0,0 +1,39 @@
+﻿namespace _1.Vehicles
+{
+    public abstract class Vehicle
+    {
+        private double fuelAmount;
+        private double fuelConsumption;
+        
+
+        public Vehicle(double fuelAmount, double fuelConsumption)
+        {
+            this.fuelAmount = fuelAmount;
+            this.fuelConsumption = fuelConsumption;
+            
+        }
+
+        public virtual void Refuel(double fuel)
+        {
+            this.fuelAmount += fuel;
+        }
+
+        public string Drive(double distance)
+        {
+            var neededFuel = this.fuelConsumption  * distance;
+
+            if (neededFuel > this.fuelAmount)
+            {
+                return $"{this.GetType().Name} needs refueling";
+            }
+
+            this.fuelAmount -= neededFuel;
+            return $"{this.GetType().Name} travelled {distance} km";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.GetType().Name}: {this.fuelAmount:F2}";
+        }
+    }
+}
